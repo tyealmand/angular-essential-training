ef40a33e89a6bd0ecb85e9eb946030317b829b4c
Tye Almand <tyealmand@hotmail.com>
2022-10-14 10:47:38 -0500

    section updates

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  @Input('mediaItem') mediaItem;
+  onDelete() {
+    console.log('deleted');
+  }
+}
